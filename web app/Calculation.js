5efa823c2b4a3c1653242eadea0f091613374724
etiti <christopherobinna27@gmail.com>
2024-07-15 19:36:20 +0200

    first commit

---
+++ web app/Calculation.js
@@ -0,0 +1,13 @@
+const addition = (num1, num2)=>{
+
+    const result = num1 + num2;
+    return result
+
+}
+const substraction= (num1, num2)=>{
+
+    const result = num1 - num2;
+    return result
+
+}
+module.exports ={addition, substraction}
---
+++ web app/calculation.test.js
@@ -0,0 +1,31 @@
+const calculate = require('./Calculation');
+// adition test
+test('adds 1 + 2 to equal 3', () => {
+  expect(calculate.addition(1, 2)).toBe(3);
+});
+
+test('adds -1 + -1 to equal -2', () => {
+  expect(calculate.addition(-1, -1)).toBe(-2);
+});
+
+test('adds 0 + 0 to equal 0', () => {
+  expect(calculate.addition(0, 0)).toBe(0);
+});
+
+// substraction test
+
+test('adds 2-1 to equal 3', () => {
+    expect(calculate.substraction(2, 1)).toBe(1);
+  });
+  
+  test('adds -1 - -1 to equal -2', () => {
+    expect(calculate.substraction(-1, -1)).toBe(0);
+  });
+  
+  test('adds 0 - 0 to equal 0', () => {
+    expect(calculate.substraction(0, 0)).toBe(0);
+  });
+
+  test('adds 1 - 2 to equal -1', () => {
+    expect(calculate.substraction(1, 2)).toBe(-1);
+  });
