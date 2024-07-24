9c86ca37b0f46697374f33833c1d2a8c809f6359
etiti <christopherobinna27@gmail.com>
2024-07-25 00:10:49 +0200

    setup k8s

--- build/k8s-worker-setup/tasks/testoutput.sh
+++
@@ -1,2 +0,0 @@
-#! bin/bash 
-kubeadm join 172.31.0.67:6443 --token wzy6df.968h5ekqlak8pdga \ 	--discovery-token-ca-cert-hash sha256:a93e9e5ab113b33db76160b568296d15bcf5ab05713399acb443e6fc89aa6f8e 
